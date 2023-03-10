2d8a9bdcf3c0e07a5c1134ea606203105d46d3c9
nguyenthanhvinh0706 <74550484+nguyenthanhvinh0706@users.noreply.github.com>
2023-03-10 15:12:34 +0700

    update like post

--- src/components/PostItemFooter.vue
+++ src/components/PostItemFooter.vue
@@ -1,25 +1,88 @@
 <template>
-    <div class="ass1-section__footer">
-        <a href="#" class="ass1-section__btn-like ass1-btn-icon"><i class="icon-Favorite_Full"></i><span>1,274</span></a>
-        <router-link v-bind:to="{ name: 'post-detail', params: { id: this.post.PID } }" href="#" class="ass1-section__btn-comment ass1-btn-icon"><i class="icon-Comment_Full"></i><span>{{ commentCount }}</span></router-link>
-  
-    </div>
-  </template>
-  
-  <script>
-  export default {
-      name: 'post-item-footer',
-      props: {
-          post: { type: Object, default: null }
-      },
-      computed: {
-          commentCount() {
-              if(this.post.count) return this.post.count;
-              return 0;
-          }
+  <div class="ass1-section__footer">
+    <a class="ass1-section__btn-like ass1-btn-icon" @click="like"
+      ><i class="icon-Favorite_Full"></i><span>{{ likes }}</span></a
+    >
+    <router-link
+      v-bind:to="{ name: 'post-detail', params: { id: this.post.PID } }"
+      href="#"
+      class="ass1-section__btn-comment ass1-btn-icon"
+      ><i class="icon-Comment_Full"></i
+      ><span>{{ commentCount }}</span></router-link
+    >
+  </div>
+</template>
+
+<script>
+import { PostService } from "../services/post.service";
+import { mapGetters } from "vuex";
+export default {
+  name: "post-item-footer",
+  props: {
+    post: { type: Object, default: null }
+  },
+  data() {
+    return {
+      postDetails: {}
+    };
+  },
+  computed: {
+    ...mapGetters(["isLogin", "currentUser"]),
+    commentCount() {
+      if (this.post.count) return this.post.count;
+      return 0;
+    },
+    likes() {
+      let total = 0;
+      if (this.postDetails && this.postDetails.likes)
+        total = this.postDetails.likes.length;
+
+      return total;
+    }
+  },
+  methods: {
+    async like() {
+      if (this.isLogin) {
+        const postId = this.post.PID;
+        const userId = this.currentUser.USERID.toString();
+
+        // add like
+        if (this.postDetails && !this.postDetails.likes.includes(userId)) {
+          await PostService.likePost({
+            postId,
+            userId
+          });
+
+          this.postDetails.likes.push(userId);
+        }
+        // unlike
+        else {
+          await PostService.unlikePost({
+            postId,
+            userId
+          });
+
+          const index = this.postDetails.likes.findIndex(
+            item => item === userId
+          );
+          this.postDetails.likes.splice(index, 1);
+        }
+      } else {
+        this.$router.push("/login");
       }
+    },
+    async getPostDetails() {
+      this.postDetails = (await PostService.getPost(this.post.PID)).data();
+      if (!this.postDetails)
+        this.postDetails = {
+          likes: []
+        };
+    }
+  },
+  async mounted() {
+    await this.getPostDetails();
   }
-  </script>
-  
-  <style></style>
-  
+};
+</script>
+
+<style></style>
--- src/services/post.service.js
+++ src/services/post.service.js
@@ -1,4 +1,12 @@
 import { COLLECTION, db, serverTimestamp } from "../firebase";
+import firebase from "firebase/app"
+
+const getPost = async postId => {
+  return await db
+    .collection(COLLECTION.POST)
+    .doc(postId)
+    .get();
+};
 
 const addComment = async (payload = { msg: "", postId: "", userId: "" }) => {
   return await db
@@ -14,6 +22,33 @@ const addComment = async (payload = { msg: "", postId: "", userId: "" }) => {
       { merge: true }
     );
 };
+
+const likePost = async (payload = { postId: "", userId: "" }) => {
+  return await db
+    .collection(COLLECTION.POST)
+    .doc(payload.postId)
+    .set(
+      {
+        likes: firebase.firestore.FieldValue.arrayUnion(payload.userId)
+      },
+      { merge: true }
+    );
+};
+const unlikePost = async (payload = { postId: "", userId: "" }) => {
+  return await db
+    .collection(COLLECTION.POST)
+    .doc(payload.postId)
+    .set(
+      {
+        likes: firebase.firestore.FieldValue.arrayRemove(payload.userId)
+      },
+      { merge: true }
+    );
+};
+
 export const PostService = {
-  addComment
+  addComment,
+  likePost,
+  unlikePost,
+  getPost
 };
