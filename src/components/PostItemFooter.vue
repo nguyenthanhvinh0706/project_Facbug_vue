78ed17ee17be86b0c0fa4c91ab712bf8f28d1598
nguyenthanhvinh0706 <74550484+nguyenthanhvinh0706@users.noreply.github.com>
2023-03-16 13:51:19 +0700

    fix bug like & dislike

--- src/components/PostItemFooter.vue
+++ src/components/PostItemFooter.vue
@@ -1,23 +1,27 @@
 <template>
   <div class="ass1-section__footer">
-    <a class="ass1-section__btn-like ass1-btn-icon" @click="like"
-      ><i class="fa fa-thumbs-o-up" aria-hidden="true"></i><span class="like-count">{{ likes }}</span></a
+    <a class="ass1-section__btn-like ass1-btn-icon" @click="like">
+      <i class="fa fa-thumbs-up" :class="{ liked }" aria-hidden="true"></i>
+      <span class="like-count">{{ likes }}</span></a
     >
     <a class="ass1-section__btn-like ass1-btn-icon" @click="dislike"
-      ><i class="fa fa-thumbs-o-down" aria-hidden="true"></i><span class="dislike-count">{{ dislikes }}</span></a
+      >
+      <i class="fa fa-thumbs-down" :class="{ disliked }" aria-hidden="true"></i>
+     <span class="dislike-count">{{ dislikes }}</span></a
     >
     <router-link
       v-bind:to="{ name: 'post-detail', params: { id: this.post.PID } }"
       href="#"
       class="ass1-section__btn-comment ass1-btn-icon"
-      ><i class="fa fa-comment" aria-hidden="true"></i><span class="commentCount">{{ commentCount }}</span></router-link
+      ><i class="fa fa-comment" aria-hidden="true"></i
+      ><span class="commentCount">{{ totalComments }}</span></router-link
     >
   </div>
 </template>
 
 <script>
 import { PostService } from "../services/post.service";
-import { mapGetters } from "vuex";
+import { mapActions, mapGetters } from "vuex";
 export default {
   name: "post-item-footer",
   props: {
@@ -25,15 +29,12 @@ export default {
   },
   data() {
     return {
-      postDetails: {}
+      postDetails: {},
+      totalComments: 0
     };
   },
   computed: {
     ...mapGetters(["isLogin", "currentUser"]),
-    commentCount() {
-      if (this.post.count) return this.post.count;
-      return 0;
-    },
     likes() {
       let total = 0;
       if (this.postDetails && this.postDetails.likes)
@@ -47,16 +48,37 @@ export default {
         total = this.postDetails.dislikes.length;
 
       return total;
+    },
+    liked() {
+      const userId = this.currentUser && this.currentUser.USERID.toString();
+      return (
+        this.postDetails &&
+        this.postDetails.likes &&
+        this.postDetails.likes.includes(userId)
+      );
+    },
+    disliked() {
+      const userId = this.currentUser && this.currentUser.USERID.toString();
+      return (
+        this.postDetails &&
+        this.postDetails.dislikes &&
+        this.postDetails.dislikes.includes(userId)
+      );
     }
   },
   methods: {
+    ...mapActions(["getListCommentByPostid"]),
     async like() {
       if (this.isLogin) {
         const postId = this.post.PID;
         const userId = this.currentUser.USERID.toString();
 
         // add like
-        if (this.postDetails && !this.postDetails.likes.includes(userId)) {
+        if (
+          this.postDetails &&
+          this.postDetails.likes &&
+          !this.postDetails.likes.includes(userId)
+        ) {
           await PostService.likePost({
             postId,
             userId
@@ -86,7 +108,11 @@ export default {
         const userId = this.currentUser.USERID.toString();
 
         // add dislike
-        if (this.postDetails && !this.postDetails.dislikes.includes(userId)) {
+        if (
+          this.postDetails &&
+          this.postDetails.dislikes &&
+          !this.postDetails.dislikes.includes(userId)
+        ) {
           await PostService.dislikePost({
             postId,
             userId
@@ -111,24 +137,39 @@ export default {
       }
     },
     async getPostDetails() {
-      this.postDetails = (await PostService.getPost(this.post.PID)).data();
-      if (!this.postDetails)
-        this.postDetails = {
-          likes: []
-        };
-    },
-    async getPostDetails() {
-      this.postDetails = (await PostService.getPost(this.post.PID)).data();
-      if (!this.postDetails)
-        this.postDetails = {
+      this.postDetails =
+        (await PostService.getPost(this.post.PID)).data() || {};
+      this.postDetails = Object.assign(
+        {
+          likes: [],
           dislikes: []
-        };
+        },
+        this.postDetails
+      );
+    },
+    async getPostComments() {
+      try {
+        const postId = this.post.PID;
+        const response = await this.getListCommentByPostid(postId);
+        this.totalComments = response.comments.length;
+      } catch (error) {
+        this.totalComments = 0;
+      }
     }
   },
   async mounted() {
     await this.getPostDetails();
+    await this.getPostComments();
   }
 };
 </script>
 
-<style></style>
+<style lang="css">
+.liked {
+  color: #174174 !important;
+}
+
+.disliked {
+  color: #CC0000 !important;
+}
+</style>
